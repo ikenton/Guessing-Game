19505919e23d5f4c69585ffecba5101882eb9cff
Isabella <imkenton@gmail.com>
2021-09-30 09:38:58 -0700

    Outline and a general main method

---
+++ game.c
@@ -0,0 +1,34 @@
+/* 1. Ask for input from the user
+big while loop
+main menu
+take in arguements 1,2,or 3 via scan
+big while loop
+1 starts the game
+2. increases the max num
+3 ends program
+
+while(1)
+ask for a number 1 - 10
+scan for answer
+run a random num generator from 0-9 +1 (rand() % 9) +1
+compare the answers
+if correct this while loop ends and we end up back at main menu
+else "too low/high" continues while loop
+else if enter q while loop ends
+
+while(2)
+print current max and scan for new max 0 <= x <= max (10)
+ends goes back to guessing (while(1))
+
+while(3)
+print out game over 
+ends 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void main(int argc, char *argv[]){
+
+    
+}
