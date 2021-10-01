3b1648cb5b9531b73e6a6252060a06baf30d54a1
Isabella <imkenton@gmail.com>
2021-09-30 19:39:09 -0700

    edited some errors in my code

--- game.c
+++ game.c
@@ -6,7 +6,6 @@ big while loop
 1 starts the game
 2. increases the max num
 3 ends program
-
 while(1)
 ask for a number 1 - 10
 scan for answer
@@ -15,11 +14,9 @@ compare the answers
 if correct this while loop ends and we end up back at main menu
 else "too low/high" continues while loop
 else if enter q while loop ends
-
 while(2)
 print current max and scan for new max 0 <= x <= max (10)
 ends goes back to guessing (while(1))
-
 while(3)
 print out game over 
 ends 
@@ -33,9 +30,7 @@ void main(int argc, char *argv[]){
     int randNum;
     int ans;
     int guess;
-    int user_max;
-    char str[2];
-    FILE *fp;
+    //char c;
     do{
         printf("MAIN MENU\n************************\nChose One of the following:\n1: Start the game\n2. Set max number\n3. Quit game\n");
         
@@ -44,22 +39,22 @@ void main(int argc, char *argv[]){
         if(c == 'q'){
             break;
         }
-        if(ans == 1 && c != 'q'){
+        if(ans == 1 ){
             srand(time(0));
             randNum = rand() % max + 1;
             do{
                 
-                
+                printf("Enter a number between 1 and %i\n", max+1);
                 scanf("%d", &guess);
                 char c = getchar();
                 if(c == 'q'){
                     printf("quit.... taking user back to MAIN MENU\n");
                     break;
                 }
-                if(guess > randNum+1 ){
+                if(guess > randNum ){
                     
                     printf("oops too high! Try again: \n");
-                } else if(guess < randNum+1 ){
+                } else if(guess < randNum ){
                     printf("Oops too low! Try again: \n");
                 }
         
@@ -72,13 +67,18 @@ void main(int argc, char *argv[]){
         
          else if(ans == 2){
             printf("Choose a new max between 1-10\n");
-            scanf("%d", &user_max);
+            scanf("%d", &max);
             char c = getchar();
-            if(c == 'q'){
-                break;
-            }
-            changeMax(user_max);
-
+            
+                //stays between 0-9 n
+                while(max-1 < 0 && max-1 > 9){
+                    if(c == 'q'){
+                        break;
+                    }
+                    printf("The number you have chosen is %d. This number is out of bounds \n", max);
+                    printf("Please choose a new max between 1-10\n");
+                }
+                printf("The new max is %i.", max);
         }
         else if(ans == 3 || c == 'q'){
             //TODO game over script
@@ -90,17 +90,8 @@ void main(int argc, char *argv[]){
     //print another game over statement here
 }
 
-int changeMax(int input){
+int saveNum(int numToSave){
+    FILE *fp;
+
     
-            char str[2];
-                //stays between 0-9 n
-                while(input-1 < 0 && input-1 > 9){
-                    if(c == 'q'){
-                        break;
-                    }
-                    printf("The number you have chosen is %d. This number is out of bounds \n", max);
-                    printf("Please choose a new max between 1-10\n");
-                }
-                printf("The new max is %i.", max);
-                
 }
